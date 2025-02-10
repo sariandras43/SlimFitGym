b0c900545eeb946b51576c9ea7cf912ea75b902c
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-02-10 21:56:17 +0100

    base hero section design #20

--- Frontend/src/app/main-page/main-page.component.ts
+++ Frontend/src/app/main-page/main-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from "../hero/hero.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [HeroComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
