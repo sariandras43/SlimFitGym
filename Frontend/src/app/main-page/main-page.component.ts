eeed8d81c746be68c4c7fbe2bbc1fe9b24d57135
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2025-02-11 08:24:02 +0100

    counter bubble imported to main page #24

--- Frontend/src/app/main-page/main-page.component.ts
+++ Frontend/src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../hero/hero.component";
+import { CounterBubbleComponent } from "../counter-bubble/counter-bubble.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeroComponent],
+  imports: [HeroComponent, CounterBubbleComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
