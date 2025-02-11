2822d7d26cb2fd5d3da7f92d1538ac7430de6b19
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2025-02-11 09:21:34 +0100

    svg uploaded and imported for the subscriptions component #26

--- Frontend/src/app/main-page/main-page.component.ts
+++ Frontend/src/app/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../hero/hero.component";
 import { CounterBubbleComponent } from "../counter-bubble/counter-bubble.component";
+import { SubscriptionsComponent } from "../subscriptions/subscriptions.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeroComponent, CounterBubbleComponent],
+  imports: [HeroComponent, CounterBubbleComponent, SubscriptionsComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
