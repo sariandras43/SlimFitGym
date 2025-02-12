ee1c30ebd6e4828f2adedc81b513d2be667d76d3
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-02-12 15:12:04 +0100

    router link for subscriptions #26

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
+import { SubscriptionsPageComponent } from './subscriptions-page/subscriptions-page.component';
 
 export const routes: Routes = [
     { path: '', component: MainPageComponent },
+    { path: 'subscriptions', component: SubscriptionsPageComponent },
 ];
