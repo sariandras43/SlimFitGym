78e1d70262c73f5590f26c5d39f21ac46e187189
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2025-02-14 10:03:03 +0100

    sign up page basic design #31

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
 import { SubscriptionsPageComponent } from './subscriptions-page/subscriptions-page.component';
+import { LogInPageComponent } from './log-in-page/log-in-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
 export const routes: Routes = [
     { path: '', component: MainPageComponent },
     { path: 'subscriptions', component: SubscriptionsPageComponent },
+
+    {path: 'login', component: LogInPageComponent},
+    {path: 'signup', component: SignUpPageComponent}
 ];
