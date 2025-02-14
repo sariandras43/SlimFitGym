80cdf933094c68f1f13b580dc9d564b4238de7e3
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2025-02-14 10:02:44 +0100

    log in page basic design #30

---
+++ Frontend/src/app/log-in-page/log-in-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in-page',
+  imports: [],
+  templateUrl: './log-in-page.component.html',
+  styleUrl: './log-in-page.component.scss'
+})
+export class LogInPageComponent {
+
+}
