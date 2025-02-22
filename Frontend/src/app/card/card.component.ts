d3675d483ccd42ef8c37dd07c915aba9932005d6
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-02-22 10:51:05 +0100

    card size now hsa to be configured on the element giving it more flexibility and added optional properties#27

--- Frontend/src/app/card/card.component.ts
+++ Frontend/src/app/card/card.component.ts
@@ -1,8 +1,10 @@
+import { NgClass } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [NgClass,RouterLink],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
@@ -14,5 +16,10 @@ export class CardComponent {
    btnText = input<string>();
    linkBtnText = input<string>()
    linkTo = input<string>()
+   imgSrc = input<string>()
+   description = input<string>()
+   isClickable = input<boolean>()
+   viewTransition= input<string>()
 
+  
 }
