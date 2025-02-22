ed84aca4159b58fa53b51c2e5f80c8c7d347b84e
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-02-22 10:56:19 +0100

    created the cards for the room-details #37

---
+++ Frontend/src/app/room-detail/room-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeroComponent } from "../hero/hero.component";
+import { CounterBubbleComponent } from "../counter-bubble/counter-bubble.component";
+import { CardComponent } from "../card/card.component";
+
+@Component({
+  selector: 'app-room-detail',
+  imports: [HeroComponent, CounterBubbleComponent, CardComponent],
+  templateUrl: './room-detail.component.html',
+  styleUrl: './room-detail.component.scss'
+})
+export class RoomDetailComponent {
+
+}
