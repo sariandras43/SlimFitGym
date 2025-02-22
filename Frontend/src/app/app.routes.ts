ebcbb8ffeafea04393c3f411c0961efc10964ad2
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-02-22 10:57:22 +0100

    routes for rooms and room details added#19

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
 import { SubscriptionsPageComponent } from './subscriptions-page/subscriptions-page.component';
 import { LogInPageComponent } from './log-in-page/log-in-page.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { RoomDetailComponent } from './room-detail/room-detail.component';
 
 export const routes: Routes = [
     { path: '', component: MainPageComponent },
     { path: 'subscriptions', component: SubscriptionsPageComponent },
+    { path: 'rooms', component: RoomsComponent },
+    {path: 'rooms/pelda', component: RoomDetailComponent},
 
     {path: 'login', component: LogInPageComponent},
     {path: 'signup', component: SignUpPageComponent}
