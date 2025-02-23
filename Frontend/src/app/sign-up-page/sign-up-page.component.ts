64946c98f9c9e86db2a5829cc517de5f44c5e247
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-02-23 21:57:17 +0100

    every field is now required for sign up #30

--- Frontend/src/app/sign-up-page/sign-up-page.component.ts
+++ Frontend/src/app/sign-up-page/sign-up-page.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
 import {  RouterModule } from '@angular/router';
-
+import {FormGroup,FormControl, Validators} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
+import {Component, inject} from '@angular/core';
 @Component({
   selector: 'app-sign-up-page',
-  imports: [RouterModule],
+  imports: [RouterModule,ReactiveFormsModule],
   templateUrl: './sign-up-page.component.html',
   styleUrl: './sign-up-page.component.scss'
 })
 export class SignUpPageComponent {
+  private formBuilder = inject(FormBuilder);
+  profileForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    phone: ['', Validators.required],
+    password: ['', Validators.required],
+    passwordAgain: ['', Validators.required],
+    
+  });
+
 
 }
