1e848832cc01baa7e6d1bc3a21b94558ced99b68
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2025-02-28 09:21:47 +0100

    room-detai now displays event #37 #40

--- Frontend/src/app/room-detail/room-detail.component.ts
+++ Frontend/src/app/room-detail/room-detail.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "../hero/hero.component";
 import { CounterBubbleComponent } from "../counter-bubble/counter-bubble.component";
 import { CardComponent } from "../card/card.component";
+import { EventsComponent } from "../events/events.component";
 
 @Component({
   selector: 'app-room-detail',
-  imports: [HeroComponent, CounterBubbleComponent, CardComponent],
+  imports: [HeroComponent, CounterBubbleComponent, CardComponent, EventsComponent],
   templateUrl: './room-detail.component.html',
   styleUrl: './room-detail.component.scss'
 })
