92250402abdac9c838cd0e4943769d2b4e06f2a2
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-03-03 21:10:20 +0100

    rework of the machines section #37

--- Frontend/src/app/room-detail/room-detail.component.ts
+++ Frontend/src/app/room-detail/room-detail.component.ts
@@ -3,13 +3,14 @@ import { HeroComponent } from "../hero/hero.component";
 import { CounterBubbleComponent } from "../counter-bubble/counter-bubble.component";
 import { CardComponent } from "../card/card.component";
 import { EventsComponent } from "../events/events.component";
+import { MachinesComponent } from "../machines/machines.component";
 
 @Component({
   selector: 'app-room-detail',
-  imports: [HeroComponent, CounterBubbleComponent, CardComponent, EventsComponent],
+  imports: [HeroComponent, CounterBubbleComponent, CardComponent, EventsComponent, MachinesComponent],
   templateUrl: './room-detail.component.html',
   styleUrl: './room-detail.component.scss'
 })
 export class RoomDetailComponent {
-gepek = Array<string>(10)
+  machines = Array<string>(10)
 }
