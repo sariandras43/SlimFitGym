61c04c29d7cf6f2d33166fc678aed09312902dd2
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-03-06 20:37:39 +0100

    User page started #51

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { RoomsComponent } from './Pages/rooms/rooms.component';
 import { RoomDetailComponent } from './Components/room-detail/room-detail.component';
 import { MachinesPageComponent } from './Pages/machines-page/machines-page.component';
 import { TrainigsPageComponent } from './Pages/trainigs-page/trainigs-page.component';
+import { UserPageComponent } from './Pages/user-page/user-page.component';
 
 export const routes: Routes = [
     { path: '', component: MainPageComponent },
@@ -15,6 +16,7 @@ export const routes: Routes = [
     {path: 'rooms/pelda', component: RoomDetailComponent},
     {path: 'machines', component: MachinesPageComponent},
     {path: 'training', component: TrainigsPageComponent},
+    {path: 'user', component: UserPageComponent},
     
     {path: 'login', component: LogInPageComponent},
     {path: 'signup', component: SignUpPageComponent}
