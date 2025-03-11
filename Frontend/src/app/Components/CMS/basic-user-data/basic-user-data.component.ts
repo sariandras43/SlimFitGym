593509d920842b631b7d86003887828c6ea9554e
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-03-11 20:47:34 +0100

    all of the user data displayed the delete and the modify should be created #52

--- Frontend/src/app/Components/CMS/basic-user-data/basic-user-data.component.ts
+++ Frontend/src/app/Components/CMS/basic-user-data/basic-user-data.component.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { UserModel } from '../../../Models/user.model';
 import { FormsModule } from '@angular/forms';
 import { PassModel } from '../../../Models/pass.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-basic-user-data',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './basic-user-data.component.html',
   styleUrl: './basic-user-data.component.scss',
 })
