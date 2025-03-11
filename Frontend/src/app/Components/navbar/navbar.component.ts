e6e2bcdab170ebf44137d7bf158923a2e2b1d286
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-03-11 20:49:04 +0100

    login sign up now transforms to profile pic if logged in #19

--- Frontend/src/app/Components/navbar/navbar.component.ts
+++ Frontend/src/app/Components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import {  RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { UserModel } from '../../Models/user.model';
+import {  UserService } from '../../Services/user.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ RouterModule],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
+  loggedInUser: UserModel | undefined;
+  
+  constructor(authService: UserService) {
+    
+    authService.loggedInUser$.subscribe((user) => {
+      this.loggedInUser = user;
+    });
+  }
 }
