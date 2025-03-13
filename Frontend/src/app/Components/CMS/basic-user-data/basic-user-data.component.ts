956ba793a84270d71b4059df57683869f2974e4a
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-03-13 20:57:39 +0100

    can modify except password #52

--- Frontend/src/app/Components/CMS/basic-user-data/basic-user-data.component.ts
+++ Frontend/src/app/Components/CMS/basic-user-data/basic-user-data.component.ts
@@ -12,7 +12,7 @@ import { UserService } from '../../../Services/user.service';
   styleUrl: './basic-user-data.component.scss',
 })
 export class BasicUserDataComponent {
-  @Input() user: UserModel ={
+  @Input() user: UserModel = {
     email: '',
     id: 0,
     imageUrl: '',
@@ -22,17 +22,54 @@ export class BasicUserDataComponent {
     token: '',
     validTo: '',
   };
-  
+
   @Input() loggedInUserPass: PassModel | undefined;
 
   /**
    *
    */
-  constructor(private userService : UserService, private router: Router) {
-
-  }
-  logout(){
+  constructor(private userService: UserService, private router: Router) {}
+  logout() {
     this.userService.logout();
     this.router.navigate(['/']);
+  }
+  
+  updateUser() {
+    const user =
+    localStorage.getItem('loggedInUser') ||
+    sessionStorage.getItem('loggedInUser');
+    if (!user) {
+      return;
+    }
+    const parsedUser: UserModel = JSON.parse(user);
+    let updateUserData: UserModel = {id: parsedUser.id};
+    
+    if (!(updateUserData.email == '' || this.user.email == parsedUser.email)) {
+      updateUserData.email = this.user.email;
+    }
+    if (!(updateUserData.imageUrl == '' || this.user.imageUrl == parsedUser.imageUrl)) {
+      updateUserData.imageUrl = this.user.imageUrl;
+    }
+    if (!(updateUserData.name == '' || this.user.name == parsedUser.name)) {
+      updateUserData.name = this.user.name;
+    }
+    if (!(updateUserData.phone == '' || this.user.phone == parsedUser.phone)) {
+      updateUserData.phone = this.user.phone;
+    }
+    this.userService.updateUser(updateUserData).subscribe({
+      next: (response) => {
+        if (response) {
+          console.log(response)
+        }
+        else {
+          console.log( "Helytelen email cím vagy jelszó!");
+        }
+      },
+      error: (error) => {
+        console.log(error.error.message ?? error.message);
+      }
+    });
+
+    
   }
 }
