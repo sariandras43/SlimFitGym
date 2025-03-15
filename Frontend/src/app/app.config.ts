1c5f95b0042c435b3483db10af446607a5373d28
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-03-15 22:13:38 +0100

    counter bubble redesign #25

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync()
   ],
 };
