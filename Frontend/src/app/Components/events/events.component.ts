bb7f6e5cebdd2402c9c4a7c7a299fbec43a4d21c
Péterffy Zoltán <peterffy.zoltan9@gmail.com>
2025-03-20 09:10:50 +0100

    now displays the inputted events correcty #40

--- Frontend/src/app/Components/events/events.component.ts
+++ Frontend/src/app/Components/events/events.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TrainingModel } from '../../Models/training.model';
 
 @Component({
   selector: 'app-events',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './events.component.scss'
 })
 export class EventsComponent {
-  esemenyek = Array<string>(10)
+  @Input() events : Array<TrainingModel> = Array<TrainingModel>(10)
+  displayDate(training: TrainingModel) {
+    const {trainingStart,trainingEnd } = training;
+    const startString = `${trainingStart.getFullYear()}.${trainingStart.getDate()}.${trainingStart.getDay()} ${trainingStart.getHours()}:${trainingStart.getHours()} - `;
+    if (trainingStart.getDay() == trainingEnd.getDay()) {
+      return startString + `${trainingEnd.getHours()}:${trainingEnd.getMinutes()}`;
+    }
+    return startString + `${trainingEnd.getFullYear()}.${trainingEnd.getDate()}.${trainingEnd.getDay()} ${trainingEnd.getHours()}:${trainingEnd.getHours()}`;
+  }
 }
