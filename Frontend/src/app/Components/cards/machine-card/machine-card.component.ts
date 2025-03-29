1e05170eca97df9a6797806e89d3a4a9d0986987
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-03-29 23:47:20 +0100

    flip animation and general styling for machine card #43

--- Frontend/src/app/Components/cards/machine-card/machine-card.component.ts
+++ Frontend/src/app/Components/cards/machine-card/machine-card.component.ts
@@ -1,13 +1,36 @@
 import { Component, Input } from '@angular/core';
-import { GeneralPurposeCardComponent } from "../general-purpose-card/general-purpose-card.component";
 import { MachineModel } from '../../../Models/machine.model';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-machine-card',
-  imports: [GeneralPurposeCardComponent],
+  imports: [ NgClass],
   templateUrl: './machine-card.component.html',
   styleUrl: './machine-card.component.scss'
 })
 export class MachineCardComponent {
+flipCard() {
+  if(!this.machine?.imageUrls[1]) return
+  this.flipped = !this.flipped;
+}
   @Input() machine: MachineModel|undefined;
+  flipped = false;
+  showFullDescription = false;
+  isDescriptionOverflow = false;
+
+  ngAfterViewInit() {
+    this.checkDescriptionOverflow();
+  }
+
+  toggleDescription(event: Event) {
+    event.stopPropagation();
+    this.showFullDescription = !this.showFullDescription;
+  }
+
+  private checkDescriptionOverflow() {
+    const descElement = document.querySelector('.description');
+    if (descElement) {
+      this.isDescriptionOverflow = descElement.scrollHeight > descElement.clientHeight;
+    }
+  }
 }
