4782b13dc9a1dbb90b90a8d73726c6555da12bb9
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-04-07 16:58:19 +0200

    subscribed trainings done #97

--- Frontend/src/app/Components/CMS/subscribed-trainings/subscribed-trainings.component.ts
+++ Frontend/src/app/Components/CMS/subscribed-trainings/subscribed-trainings.component.ts
@@ -3,80 +3,66 @@ import { TrainingModel } from '../../../Models/training.model';
 import { UserModel } from '../../../Models/user.model';
 import { TrainingService } from '../../../Services/training.service';
 import { UserService } from '../../../Services/user.service';
+import Utils from '../../../utils/util';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-subscribed-trainings',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './subscribed-trainings.component.html',
   styleUrl: './subscribed-trainings.component.scss'
 })
 export class SubscribedTrainingsComponent {
- 
- subscribe(training: TrainingModel) {
-     this.trainingService.subscribeToTraining(training.id).subscribe({
-       next: () => {},
-       error: (err) => {
-         console.log(err);
-       },
-     });
-   }
-   unsubscribe(training: TrainingModel) {
-     this.trainingService.unsubscribeFromTraining(training.id).subscribe({
-       next: () => {},
-       error: (err) => {
-         console.log(err);
-       },
-     });
-   }
+  searchValue = '';
+
+  search(){
+    console.log(this.searchValue)
+    this.displayTrainings = this.trainings.filter(t=> t.name.toLowerCase().includes(this.searchValue)
+  || t.trainer.toLowerCase().includes(this.searchValue))
+  }
+
+
+
    trainings: TrainingModel[] = [];
+   displayTrainings: TrainingModel[] = [];
    user: UserModel | undefined;
  
    constructor(
-     private trainingService: TrainingService,
-     userService: UserService
+     private trainingService: TrainingService
    ) {
-     trainingService.allTrainings$.subscribe({
+     trainingService.subscribedTrainings$.subscribe({
        next: (t) => {
          if (t) {
-           this.trainings = t;
+           this.trainings = t.sort(training => training.trainingStart.getTime());
+           this.displayTrainings = [...this.trainings];
+
          }
        },
        error: (err) => {
          console.log(err);
        },
      });
-     userService.loggedInUser$.subscribe((s) => {
-       if (s) this.user = s;
-     });
    }
  
  
    displayDate(training: TrainingModel): string {
-     const { trainingStart, trainingEnd } = training;
-     if (!(trainingStart instanceof Date) || !(trainingEnd instanceof Date))
-       return '';
- 
-     const pad = (num: number) => num.toString().padStart(2, '0');
- 
-     const startDate = `${trainingStart.getFullYear()}.${
-       trainingStart.getMonth() + 1
-     }.${trainingStart.getDate()}`;
-     const startTime = `${pad(trainingStart.getHours())}:${pad(
-       trainingStart.getMinutes()
-     )}`;
- 
-     if (trainingStart.toDateString() === trainingEnd.toDateString()) {
-       return `${startDate} ${startTime} - ${pad(trainingEnd.getHours())}:${pad(
-         trainingEnd.getMinutes()
-       )}`;
-     } else {
-       const endDate = `${trainingEnd.getFullYear()}.${
-         trainingEnd.getMonth() + 1
-       }.${trainingEnd.getDate()}`;
-       const endTime = `${pad(trainingEnd.getHours())}:${pad(
-         trainingEnd.getMinutes()
-       )}`;
-       return `${startDate} ${startTime} - ${endDate} ${endTime}`;
-     }
+     return Utils.displayDate(training);
    }
+
+   subscribe(training: TrainingModel) {
+    this.trainingService.subscribeToTraining(training.id).subscribe({
+      next: () => {},
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+  unsubscribe(training: TrainingModel) {
+    this.trainingService.unsubscribeFromTraining(training.id).subscribe({
+      next: () => {},
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
