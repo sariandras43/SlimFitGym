393dbf34cb7f46c0239811caf90f0a997fa2217f
PeterffyZoli <peterffy.zoltan9@gmail.com>
2025-04-12 01:17:01 +0200

    loading for sign up and log in #109

--- Frontend/src/app/Pages/log-in-page/log-in-page.component.ts
+++ Frontend/src/app/Pages/log-in-page/log-in-page.component.ts
@@ -1,47 +1,51 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import {  UserService } from '../../Services/user.service';
+import { UserService } from '../../Services/user.service';
 import { FormsModule } from '@angular/forms';
 import { UserModel } from '../../Models/user.model';
+import { ButtonLoaderComponent } from '../../Components/button-loader/button-loader.component';
 
 @Component({
   selector: 'app-log-in-page',
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, ButtonLoaderComponent],
   templateUrl: './log-in-page.component.html',
-  styleUrl: './log-in-page.component.scss'
+  styleUrl: './log-in-page.component.scss',
 })
 export class LogInPageComponent {
+  loading = false;
 
-  
   model = {
     email: '',
     password: '',
-    rememberMe: false
-  }
+    rememberMe: false,
+  };
   errorMessage = '';
 
-  constructor(
-    private userService: UserService,
-    private router: Router
-  ) { }
+  constructor(private userService: UserService, private router: Router) {}
 
   login() {
     if (!this.model.email || !this.model.password) {
-      this.errorMessage = "Email cím és jelszó megadása kötelező!";
+      this.errorMessage = 'Email cím és jelszó megadása kötelező!';
       return;
     }
-    this.userService.login(this.model.email, this.model.password, this.model.rememberMe).subscribe({
-      next: (response) => {
-        if (response) {
-          this.router.navigate(['user']);
-        }
-        else {
-          this.errorMessage = "Helytelen email cím vagy jelszó!";
-        }
-      },
-      error: (error) => {
-        this.errorMessage = error.error.message ?? error.message;
-      }
-    });
+    this.loading = true;
+    this.userService
+      .login(this.model.email, this.model.password, this.model.rememberMe)
+      .subscribe({
+        next: (response) => {
+          this.loading = false;
+
+          if (response) {
+            this.router.navigate(['user']);
+          } else {
+            this.errorMessage = 'Helytelen email cím vagy jelszó!';
+          }
+        },
+        error: (error) => {
+          this.loading = false;
+
+          this.errorMessage = error.error.message ?? error.message;
+        },
+      });
   }
 }
